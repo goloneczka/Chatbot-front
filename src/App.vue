71995ee9a4e92b4ca9392f0772806b5da32b7193
Adam <adam.krzanowski1998@gmail.com>
2020-03-30 13:14:24 +0200

    Work

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,14 @@
     import AuthorizationStorage from './core/requests/AuthorizationStorage';
     import HttpRequest from './core/requests/HttpRequest';
     import Dialog from "./components/common/Dialog";
+    import WeatherService from "./core/componentServices/WeatherService";
 
     const baseUrl = 'http://localhost:9090';
 
     export const authorizationStorage = new AuthorizationStorage();
     export const httpRequest = new HttpRequest(baseUrl, authorizationStorage);
+
+    export const weatherService = new WeatherService();
 
     export default {
         name: 'App',
---
+++ src/components/categories/Weather.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+<script>
+
+    export default {
+        name: 'weather',
+        components: {},
+        data() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+<style>
+
+</style>
