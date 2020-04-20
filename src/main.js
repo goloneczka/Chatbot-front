ea33059a720bbbd36325cfb47dbcb76d45942648
Adam <adam.krzanowski1998@gmail.com>
2020-04-20 12:46:31 +0200

    Poprawka

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import i18n from './i18n'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import { routes } from './routes'
+import {routes} from './routes'
+import {authorizationStorage} from "./App";
 
 Vue.config.productionTip = false;
 
@@ -17,9 +18,19 @@ Vue.use(IconsPlugin);
 
 const router = new VueRouter({routes});
 
+router.beforeEach((to, from, next) => {
+    if (to.path.includes("/admin") && !authorizationStorage.isEmpty()) {
+        next();
+    } else if (!to.path.includes("/admin")) {
+        next();
+    } else {
+        next(false);
+    }
+});
+
 new Vue({
     render: h => h(App),
     router,
     i18n,
-}).$mount('#app')
+}).$mount('#app');
 
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,18 @@
 import AdminLogin from './components/admin/Login';
 import Home from './components/Home'
 import AdminHome from './components/admin/AdminHome';
-import { authorizationStorage } from "./App";
 
 
 export const routesNames = {
     home: '/home',
-    adminLogin: '/admin',
+    adminLogin: '/login-admin',
     adminHome: '/admin/home'
 };
 
 export const routes = [
     {path: '/', component: Home},
     {path: routesNames.adminLogin, component: AdminLogin},
-    {
-        path: routesNames.adminHome, component: AdminHome,
-        beforeEnter: ((to, from, next) => {
-            if (to.path === routesNames.adminHome && !authorizationStorage.isEmpty()) {
-                next();
-            } else {
-                next(false);
-            }
-        })
-    }
+    {path: routesNames.adminHome, component: AdminHome}
 ];
 
 
