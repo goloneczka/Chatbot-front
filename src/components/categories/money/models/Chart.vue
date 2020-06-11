fd76653135760a0a5268b547829fad4811be1e33
TheDeMuZ <jakub.musial.001@gmail.com>
2020-06-11 22:01:53 +0200

    No cześć, jestem pan wykresik! Powykresikuj mnie :D

---
+++ src/components/categories/money/models/Chart.vue
@@ -0,0 +1,13 @@
+<script>
+    import { Line } from 'vue-chartjs'
+
+    export default {
+        name: "chart",
+        extends: Line,
+        props: ['chartdata', 'options'],
+
+        mounted() {
+            this.renderChart(this.chartdata, this.options)
+        }
+    }
+</script>
--- src/core/service/FortuneService.js
+++ src/core/service/FortuneService.js
@@ -1,5 +1,4 @@
 export default class FortuneService {
-
     constructor(httpRequest) {
         this.httpRequest = httpRequest;
     }
@@ -26,5 +25,26 @@ export default class FortuneService {
 
     getFutureDataForSymbol(symbol){
         return this.httpRequest.get(`stocks/prediction/${symbol}`);
+    }
+
+    extractChartData(stocks) {
+        let axisX = []
+        let axisY = []
+
+        for (let stock of stocks) {
+            axisX.push(stock.date)
+            axisY.push(stock.value)
+        }
+
+        return {
+            labels: axisX,
+            datasets: [{
+                lineTension: 0,
+                label: stocks[0].symbol,
+                borderColor: '#2cc3ed',
+                fill: false,
+                data: axisY
+            }]
+        }
     }
 }
